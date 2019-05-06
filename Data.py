8e1bdbf06de26e3923ccdc3d018ae2d294acfb32
Toni Ginard <toniginard94@gmail.com>
2019-05-06 20:24:37 +0200

    ImageDataGenerator, fit_generator

--- Data.py
+++ Data.py
@@ -1,25 +1,50 @@
 from keras.preprocessing.image import ImageDataGenerator
+from Model import *
 
 
-def load_train_set(train_path):
+def train_input_generator(path1, path2):
     train_datagen = ImageDataGenerator(rescale=1./255)
 
-    train_generator = train_datagen.flow_from_directory(
-        train_path,
+    train_gen_left = train_datagen.flow_from_directory(
+        path1,
         target_size=(100, 100),
-        batch_size=25,
+        batch_size=20,
         class_mode='binary')
 
-    return train_generator
+    train_gen_right = train_datagen.flow_from_directory(
+        path2,
+        target_size=(100, 100),
+        batch_size=20,
+        class_mode='binary')
+
+    while True:
+        x1i = train_gen_left.next()
+        x2i = train_gen_right.next()
+        yield [x1i[0], x2i[0]], x2i[1]  # Yield both images and their mutual label
 
 
-def load_test_set(test_path):
+def train_output_generator(test_path):
     test_datagen = ImageDataGenerator(rescale=1./255)
 
     validation_generator = test_datagen.flow_from_directory(
         test_path,
         target_size=(100, 100),
-        batch_size=25,
+        batch_size=20,
         class_mode='binary')
 
     return validation_generator
+
+
+def fit(model=unet()):
+    input_generator = train_input_generator("/Users/toniginard/Desktop/TFG/Images/TrainSet/left",
+                                            "/Users/toniginard/Desktop/TFG/Images/TrainSet/right")
+
+    output_generator = train_output_generator("/Users/toniginard/Desktop/TFG/Images/TrainSet/depth")
+
+    history = model.fit_generator(
+        input_generator,
+        steps_per_epoch=100,
+        epochs=30,
+        validation_data=output_generator,
+        validation_steps=50)
+
--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-from Model import *
+from Data  import *
 from keras import optimizers
 
 
 def main():
     model = unet()
 
-    # model.compile(loss='binary_crossentropy', optimizer=optimizers.RMSprop(lr=1e-4), metrics=['acc'])
-    # model.fit()
+    model.compile(loss='binary_crossentropy', optimizer=optimizers.RMSprop(lr=1e-4), metrics=['acc'])
+
+    fit(model)
 
 
 if __name__ == "__main__":
