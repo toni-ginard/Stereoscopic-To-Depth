5b1628cedfe3d702a520872a2ad4358cc5b50164
Toni Ginard <toniginard94@gmail.com>
2019-05-08 19:27:03 +0200

    ImageDataGenerator, fit_generator

--- Data.py
+++ Data.py
@@ -1,50 +1,24 @@
 from keras.preprocessing.image import ImageDataGenerator
-from Model import *
+import os
 
 
-def train_input_generator(path1, path2):
-    train_datagen = ImageDataGenerator(rescale=1./255)
+def train_input_generator(gen, path1, path2):
+    train_gen_left = gen.flow_from_directory(path1, target_size=(64, 64), batch_size=20)
 
-    train_gen_left = train_datagen.flow_from_directory(
-        path1,
-        target_size=(100, 100),
-        batch_size=20,
-        class_mode='binary')
-
-    train_gen_right = train_datagen.flow_from_directory(
-        path2,
-        target_size=(100, 100),
-        batch_size=20,
-        class_mode='binary')
+    train_gen_right = gen.flow_from_directory(path2, target_size=(64, 64), batch_size=20)
 
     while True:
-        x1i = train_gen_left.next()
-        x2i = train_gen_right.next()
-        yield [x1i[0], x2i[0]], x2i[1]  # Yield both images and their mutual label
+        X1i = train_gen_left.next()
+        X2i = train_gen_right.next()
+        yield [X1i[0], X2i[0]], X1i[1]
 
 
-def train_output_generator(test_path):
-    test_datagen = ImageDataGenerator(rescale=1./255)
+def input_generator(path):
+    train_datagen = ImageDataGenerator(rescale=1./255)
+    input_gen = train_datagen.flow_from_directory(path, target_size=(64, 64), batch_size=20, color_mode="grayscale")
+    return input_gen
 
-    validation_generator = test_datagen.flow_from_directory(
-        test_path,
-        target_size=(100, 100),
-        batch_size=20,
-        class_mode='binary')
 
+def train_output_generator(gen, path):
+    validation_generator = gen.flow_from_directory(path, target_size=(64, 64), batch_size=20)
     return validation_generator
-
-
-def fit(model=unet()):
-    input_generator = train_input_generator("/Users/toniginard/Desktop/TFG/Images/TrainSet/left",
-                                            "/Users/toniginard/Desktop/TFG/Images/TrainSet/right")
-
-    output_generator = train_output_generator("/Users/toniginard/Desktop/TFG/Images/TrainSet/depth")
-
-    history = model.fit_generator(
-        input_generator,
-        steps_per_epoch=100,
-        epochs=30,
-        validation_data=output_generator,
-        validation_steps=50)
-
--- main.py
+++ main.py
@@ -1,14 +1,34 @@
-from Data  import *
+from Model import unet
+from Data import *
 from keras import optimizers
+import os
 
 
 def main():
+    main_path = "/Users/toniginard/Desktop/TFG/Images/TrainSet"
     model = unet()
 
-    model.compile(loss='binary_crossentropy', optimizer=optimizers.RMSprop(lr=1e-4), metrics=['acc'])
+    model.compile(loss='binary_crossentropy', optimizer=optimizers.RMSprop(lr=1e-4), metrics=['accuracy'])
 
-    fit(model)
+    os.chdir(main_path)
+    left_gen = input_generator('left')
+
+    os.chdir(main_path)
+    right_gen = input_generator('right')
+
+    os.chdir(main_path)
+    depth_gen = input_generator('depth')
+
+    history = model.fit_generator([left_gen, right_gen],
+                                  steps_per_epoch=5,
+                                  epochs=30,
+                                  validation_data=depth_gen,
+                                  validation_steps=5)
 
 
 if __name__ == "__main__":
     main()
+
+
+#  skimage-imread: carregar i passar a escala de grisos
+#  metriques entre imatges de profunditat
