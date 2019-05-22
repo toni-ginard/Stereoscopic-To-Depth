b54b89bdf53498b77ee684dd1fd51ddc5121aafb
Toni Ginard <toniginard94@gmail.com>
2019-05-22 15:57:27 +0200

    Validation data, prediction.

--- Data.py
+++ Data.py
@@ -2,28 +2,29 @@ from keras.preprocessing.image import ImageDataGenerator
 from skimage import io
 from skimage import color
 from numpy import *
+import matplotlib.pyplot as plt
 
 
-def train_input_generator(gen, path1, path2):
-    train_gen_left = gen.flow_from_directory(path1, target_size=(64, 64), batch_size=20)
-
-    train_gen_right = gen.flow_from_directory(path2, target_size=(64, 64), batch_size=20)
-
-    while True:
-        X1i = train_gen_left.next()
-        X2i = train_gen_right.next()
-        yield [X1i[0], X2i[0]], X1i[1]
-
-
-def input_generator(path):
-    train_datagen = ImageDataGenerator(rescale=1./255)
-    input_gen = train_datagen.flow_from_directory(path, target_size=(64, 64), batch_size=20, color_mode="grayscale")
-    return input_gen
+# def train_input_generator(gen, path1, path2):
+#     train_gen_left = gen.flow_from_directory(path1, target_size=(64, 64), batch_size=20)
+#
+#     train_gen_right = gen.flow_from_directory(path2, target_size=(64, 64), batch_size=20)
+#
+#     while True:
+#         X1i = train_gen_left.next()
+#         X2i = train_gen_right.next()
+#         yield [X1i[0], X2i[0]], X1i[1]
 
 
-def train_output_generator(gen, path):
-    validation_generator = gen.flow_from_directory(path, target_size=(64, 64), batch_size=20)
-    return validation_generator
+# def input_generator(path):
+#     train_datagen = ImageDataGenerator(rescale=1./255)
+#     input_gen = train_datagen.flow_from_directory(path, target_size=(64, 64), batch_size=20, color_mode="grayscale")
+#     return input_gen
+
+
+# def train_output_generator(gen, path):
+#     validation_generator = gen.flow_from_directory(path, target_size=(64, 64), batch_size=20)
+#     return validation_generator
 
 
 def load_data(path):
@@ -32,6 +33,20 @@ def load_data(path):
     """for i in images:
         list.append(color.rgb2gray(i))"""
     for i in range(len(images)):
-        list.append(color.rgb2gray(images[i]))
+        list.append(resize(color.rgb2gray(images[i]), [64, 64]))
         list[i] = list[i][:, :, newaxis]
     return array(list)
+
+
+def validation(history):
+    loss = history['loss']
+    val_loss = history['val_loss']
+
+    epochs = range(1, len(loss) + 1)
+    plt.plot(epochs, loss, 'bo', label='Training loss')
+    plt.plot(epochs, val_loss, 'b', label='Validation loss')
+    plt.title('Training and validation loss')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend()
+    plt.show()
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from Model import unet
 from Data import *
 from keras import optimizers
 import os
+from PIL import Image
 
 
 def main():
@@ -9,15 +10,33 @@ def main():
 
     model = unet()
 
-    model.compile(loss='binary_crossentropy', optimizer=optimizers.RMSprop(lr=1e-4), metrics=['accuracy'])
+    model.compile(loss='mean_squared_error', optimizer=optimizers.RMSprop(lr=1e-4), metrics=['mse', 'mae', 'mape', 'cosine'])
 
-    left_img = load_data("/Users/toniginard/Desktop/TFG/Images/TrainSet/left/*.jpg")
-    right_img = load_data("/Users/toniginard/Desktop/TFG/Images/TrainSet/right/*.jpg")
-    depth_img = load_data("/Users/toniginard/Desktop/TFG/Images/TrainSet/depth/*.jpg")
+    # Training data
+    l_train = load_data("/Users/toniginard/Desktop/TFG/Images/Train/left/*.jpg")
+    r_train = load_data("/Users/toniginard/Desktop/TFG/Images/Train/right/*.jpg")
+    d_train = load_data("/Users/toniginard/Desktop/TFG/Images/Train/depth/*.jpg")
 
-    history = model.fit(([left_img, right_img]), depth_img, epochs=2, batch_size=2)
+    # Validation data
+    l_val = load_data("/Users/toniginard/Desktop/TFG/Images/Validation/left/*.jpg")
+    r_val = load_data("/Users/toniginard/Desktop/TFG/Images/Validation/right/*.jpg")
+    d_val = load_data("/Users/toniginard/Desktop/TFG/Images/Validation/depth/*.jpg")
 
-    model.save('stereo_to_depth.h5')
+    history = model.fit(([l_train, r_train]), d_train, epochs=10, batch_size=2, validation_data=([l_val, r_val], d_val))
+
+    # validation(history.history)
+
+    # model.save('stereo_to_depth.h5')
+
+    # Prediction data
+    l_pred = load_data("/Users/toniginard/Desktop/TFG/Images/Proves/left/*.jpg")
+    r_pred = load_data("/Users/toniginard/Desktop/TFG/Images/Proves/right/*.jpg")
+    pred = model.predict([l_pred, r_pred])
+    pred = pred[:, :, :, 0]
+
+    print("IM: ", pred[0])
+    print("SHAPE: ", pred[0].shape)
+    plt.imsave("/Users/toniginard/Desktop/im.jpg", pred[0])
 
 
 if __name__ == "__main__":
@@ -25,3 +44,4 @@ if __name__ == "__main__":
 
 
 #  metriques entre imatges de profunditat
+#  visualitzar dades, conjunts de validació i entrenament, representar imatges resultants (què fa exactament)
