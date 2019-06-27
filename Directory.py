7cb8cc7afbfb5b949644eaf8e08200730dfa3b98
Toni Ginard <toniginard94@gmail.com>
2019-06-27 18:12:03 +0200

    refractorització

--- Directory.py
+++
@@ -1,6 +0,0 @@
-import os
-
-
-def create_exp_directory(path):
-    if not os.path.exists(path):
-        os.makedirs(path)
--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 from Models.Model import *
 from Models.Unet import *
-from Models.vgg16 import *
-from Directory import *
+from Models.n2 import *
 from keras import optimizers
 from Data import *
 import os
 
 
-MAIN_PATH = "/Users/toniginard/Desktop/TFG"
-EXP_NAME = "/vgg16/Prova"
-EXP_PATH = "/Experiments" + EXP_NAME
+MAIN_PATH = "/Users/toniginard/Desktop/TFG/"
+EXP_NAME = "n2/Prova/"
+EXP_PATH = MAIN_PATH + "Entrenaments/" + EXP_NAME
 
 
 EPOCHS = 2
@@ -27,24 +26,21 @@ def main():
 
     model = get_model(in_left=(IN_SIZE, IN_SIZE), in_right=(IN_SIZE, IN_SIZE))  # unet()
 
-    model.compile(loss='mean_squared_error', optimizer=optimizers.RMSprop(lr=1e-4), metrics=['mse'])  # 'mse', 'mae', 'mape', 'cosine'
+    model.compile(loss='mean_squared_error', optimizer=optimizers.RMSprop(lr=1e-4), metrics=['mse'])
 
     # model.summary()
-
-    # preparar directoris
-    create_exp_directory("Experiments/vgg16/Prova")
 
     save_summary(model, EXP_PATH)
 
     # LOAD TRAIN DATA
-    l_train = load_data("/Images/Train/left/*.jpg", NUM_TRAIN_IMAGES, IN_SIZE)  # MAIN_PATH
-    r_train = load_data("/Images/Train/right/*.jpg", NUM_TRAIN_IMAGES, IN_SIZE)  # MAIN_PATH
-    d_train = load_data("/Images/Train/depth/*.jpg", NUM_TRAIN_IMAGES, IN_SIZE)  # MAIN_PATH
+    l_train = load_data(MAIN_PATH + "Images/Train/left/*.jpg", NUM_TRAIN_IMAGES, IN_SIZE)  # MAIN_PATH
+    r_train = load_data(MAIN_PATH + "Images/Train/right/*.jpg", NUM_TRAIN_IMAGES, IN_SIZE)  # MAIN_PATH
+    d_train = load_data(MAIN_PATH + "Images/Train/depth/*.jpg", NUM_TRAIN_IMAGES, IN_SIZE)  # MAIN_PATH
 
     # LOAD VALIDATION DATA
-    l_val = load_data("/Images/Validation/left/*.jpg", NUM_VAL_IMAGES, IN_SIZE)  # MAIN_PATH
-    r_val = load_data("/Images/Validation/right/*.jpg", NUM_VAL_IMAGES, IN_SIZE)  # MAIN_PATH
-    d_val = load_data("/Images/Validation/depth/*.jpg", NUM_VAL_IMAGES, IN_SIZE)  # MAIN_PATH
+    l_val = load_data(MAIN_PATH + "Images/Validation/left/*.jpg", NUM_VAL_IMAGES, IN_SIZE)  # MAIN_PATH
+    r_val = load_data(MAIN_PATH + "Images/Validation/right/*.jpg", NUM_VAL_IMAGES, IN_SIZE)  # MAIN_PATH
+    d_val = load_data(MAIN_PATH + "Images/Validation/depth/*.jpg", NUM_VAL_IMAGES, IN_SIZE)  # MAIN_PATH
 
     # FIT
     history = model.fit(([l_train, r_train]), d_train, epochs=EPOCHS, batch_size=BATCH_SIZE,
@@ -52,15 +48,13 @@ def main():
 
     save_validation(history.history, EXP_PATH + "/loss.png")
 
-    # model.save('stereo_to_depth.h5')
-
     # LOAD PREDICTION DATA
-    l_pred = load_data("/Images/Test/left/*.jpg", NUM_TEST_IMAGES, IN_SIZE)  # MAIN_PATH
-    r_pred = load_data("/Images/Test/right/*.jpg", NUM_TEST_IMAGES, IN_SIZE)  # MAIN_PATH
+    l_pred = load_data(MAIN_PATH + "Images/Test/left/*.jpg", NUM_TEST_IMAGES, IN_SIZE)  # MAIN_PATH
+    r_pred = load_data(MAIN_PATH + "Images/Test/right/*.jpg", NUM_TEST_IMAGES, IN_SIZE)  # MAIN_PATH
     pred = model.predict([l_pred, r_pred])
     pred = pred[:, :, :, 0]
 
-    save_predictions(EXP_PATH + "/Predictions", NUM_TEST_IMAGES, pred)
+    save_predictions(EXP_PATH + "Predictions", NUM_TEST_IMAGES, pred)
 
 
 if __name__ == "__main__":
