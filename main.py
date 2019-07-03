7001805507a66ac9878eef152fa1044af8f1608c
Toni Ginard <toniginard94@gmail.com>
2019-07-03 17:22:38 +0200

    (multiple training adjustment)

--- main.py
+++ main.py
@@ -1,69 +1,9 @@
-from Models.Unet import *
-from Models.n2 import *
-from keras import optimizers
-from Data import *
-import os
-
-
-MAIN_PATH = "/Users/toniginard/Desktop/TFG/"
-EXP_NAME = "n2/Prova/"
-EXP_PATH = MAIN_PATH + "Entrenaments/" + EXP_NAME
-
-
-EPOCHS = 2
-BATCH_SIZE = 2
-
-NUM_TRAIN_IMAGES = 10
-NUM_VAL_IMAGES = 6
-NUM_TEST_IMAGES = 2
-
-IN_SIZE = 256
+from train import *
 
 
 def main():
-    os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
-
-    model = get_model(in_left=(IN_SIZE, IN_SIZE), in_right=(IN_SIZE, IN_SIZE))  # unet()
-
-    model.compile(loss='mean_squared_error', optimizer=optimizers.RMSprop(lr=1e-4), metrics=['mse'])
-
-    # model.summary()
-
-    save_summary(model, EXP_PATH)
-
-    train_generator = get_train_generator(MAIN_PATH + "Images/Train/left",
-                                          MAIN_PATH + "Images/Train/right",
-                                          MAIN_PATH + "Images/Train/depth",
-                                          IN_SIZE,
-                                          BATCH_SIZE)
-
-    val_generator = get_train_generator(MAIN_PATH + "Images/Validation/left",
-                                        MAIN_PATH + "Images/Validation/right",
-                                        MAIN_PATH + "Images/Validation/depth",
-                                        IN_SIZE,
-                                        BATCH_SIZE)
-
-    history = model.fit_generator(train_generator,
-                                  steps_per_epoch=NUM_TRAIN_IMAGES / BATCH_SIZE,
-                                  epochs=EPOCHS,
-                                  validation_data=val_generator,
-                                  validation_steps=NUM_VAL_IMAGES / BATCH_SIZE)
-
-    save_validation(history.history, EXP_PATH + "/loss.png")
-
-    test_generator = get_test_generator(MAIN_PATH + "Images/Test/left",
-                                        MAIN_PATH + "Images/Test/right",
-                                        IN_SIZE,
-                                        BATCH_SIZE)
-
-    predictions = model.predict_generator(test_generator,
-                                          steps=NUM_TEST_IMAGES)
-
-    # save_predictions(EXP_PATH + "Predictions", NUM_TEST_IMAGES, predictions)
+    train("Prova", 2, 2, 10, 6, 2)
 
 
 if __name__ == "__main__":
     main()
-
-
-#  np.tile
---
+++ train.py
@@ -0,0 +1,51 @@
+from Models.Unet import *
+from Models.n2 import *
+from keras import optimizers
+from Data import *
+import os
+
+
+IN_SIZE = 256
+
+
+def train(exp_name, epochs, batch_size, n_train_img, n_val_img, n_test_img):
+    main_path = "/Users/toniginard/Desktop/TFG/"
+    exp_path = main_path + "Entrenaments/" + "n2/" + exp_name + "/"
+    
+    os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
+
+    model = get_model(in_left=(IN_SIZE, IN_SIZE), in_right=(IN_SIZE, IN_SIZE))  # unet()
+
+    model.compile(loss='mean_squared_error', optimizer=optimizers.RMSprop(lr=1e-4), metrics=['mse'])
+
+    save_summary(model, exp_path)
+
+    train_generator = get_train_generator(main_path + "Images/Train/left",
+                                          main_path + "Images/Train/right",
+                                          main_path + "Images/Train/depth",
+                                          IN_SIZE,
+                                          batch_size)
+
+    val_generator = get_train_generator(main_path + "Images/Validation/left",
+                                        main_path + "Images/Validation/right",
+                                        main_path + "Images/Validation/depth",
+                                        IN_SIZE,
+                                        batch_size)
+
+    history = model.fit_generator(train_generator,
+                                  steps_per_epoch=n_train_img / batch_size,
+                                  epochs=epochs,
+                                  validation_data=val_generator,
+                                  validation_steps=n_val_img / batch_size)
+
+    save_validation(history.history, exp_path + "/loss.png")
+
+    test_generator = get_test_generator(main_path + "Images/Test/left",
+                                        main_path + "Images/Test/right",
+                                        IN_SIZE,
+                                        batch_size)
+
+    # predictions = model.predict_generator(test_generator,
+    #                                       steps=n_test_img)
+
+    # save_predictions(exp_path + "Predictions", n_test_img, predictions)
