f6b3a37a37771d14009f34220483fcc3c38653a7
Toni Ginard <toniginard94@gmail.com>
2019-09-11 11:26:12 +0200

    refactoring adjustments

--- Models/n2.py
+++ Models/n2.py
@@ -1,7 +1,8 @@
-from keras import layers, Model
+from keras.models import *
+from keras import layers
 
 
-def get_model(input_channels=1, ouput_channels=1, in_left=(64, 64), in_right=(64, 64), residual_blocks=5):
+def get_model(input_channels=1, ouput_channels=1, in_left=(64,64), in_right=(64,64), residual_blocks=5):
     """ Generate an image-to-image model.
 
     Based on "Perceptual Losses for Real-Time Style Transfer and Super-Resolution", by Justin et al.
@@ -16,15 +17,21 @@ def get_model(input_channels=1, ouput_channels=1, in_left=(64, 64), in_right=(64
     assert in_right[0] % 4 == 0 and in_right[1] % 4 == 0, 'Input size must by multiple of 4'
     # Define layers
     lyrs = [
-        layers.Conv2D(filters=32, kernel_size=(9, 9), strides=1, padding='same'),
-        layers.Conv2D(filters=64, kernel_size=(3, 3), strides=2, padding='same'),
+        layers.Conv2D(filters=32, kernel_size=(9, 9), activation='relu', strides=1, padding='same',
+                      kernel_initializer='he_normal'),
+        layers.Conv2D(filters=64, kernel_size=(3, 3), activation='relu', strides=2, padding='same',
+                      kernel_initializer='he_normal'),
         layers.Dropout(0.5),
-        layers.Conv2D(filters=128, kernel_size=(3, 3), strides=2, padding='same'),
+        layers.Conv2D(filters=128, kernel_size=(3, 3), activation='relu', strides=2, padding='same',
+                      kernel_initializer='he_normal'),
     ] + [ResidualBlock(num_filters=128) for i in range(residual_blocks)] + [
-        layers.Conv2DTranspose(filters=64, kernel_size=(3, 3), strides=2, padding='same'),
-        layers.Conv2DTranspose(filters=32, kernel_size=(3, 3), strides=2, padding='same'),
+        layers.Conv2DTranspose(filters=64, kernel_size=(3, 3), activation='relu', strides=2, padding='same',
+                               kernel_initializer='he_normal'),
+        layers.Conv2DTranspose(filters=32, kernel_size=(3, 3), activation='relu', strides=2, padding='same',
+                               kernel_initializer='he_normal'),
         layers.Dropout(0.5),
-        layers.Conv2D(filters=ouput_channels, kernel_size=(9, 9), strides=1, padding='same')
+        layers.Conv2D(filters=ouput_channels, kernel_size=(9, 9), activation='relu', strides=1, padding='same',
+                      kernel_initializer='he_normal')
     ]
     # Define input-output logic. Assume channels last (tensorflow)
     left = layers.Input(shape=in_left + (input_channels,))
@@ -43,10 +50,12 @@ class ResidualBlock:
         self._n = num_filters
 
     def __call__(self, tensor):
-        out = layers.Conv2D(self._n, kernel_size=(3, 3), strides=(1, 1), padding='same')(tensor)
+        out = layers.Conv2D(self._n, kernel_size=(3, 3), activation='relu', strides=(1, 1), padding='same',
+                            kernel_initializer='he_normal')(tensor)
         out = layers.BatchNormalization()(out)
         out = layers.Activation('relu')(out)
-        out = layers.Conv2D(self._n, kernel_size=(3, 3), strides=(1, 1), padding='same')(out)
+        out = layers.Conv2D(self._n, kernel_size=(3, 3), activation='relu', strides=(1, 1), padding='same',
+                            kernel_initializer='he_normal')(out)
         out = layers.BatchNormalization()(out)
         out = layers.Add()([tensor, out])   # Addition with original input tensor
         out = layers.Activation('relu')(out)
--- Train.py
+++ Train.py
@@ -1,48 +1,61 @@
 from Models.n2 import *
+from keras.callbacks import EarlyStopping
 from Data import *
 import os
+import keras.backend as k
 
 
-def train(exp_name, epochs, batch_size, in_size, n_train_img, n_val_img, n_test_img, pretrained_weights, opt):
-    main_path = "/Users/toniginard/TFG/"
-    exp_path = main_path + "Entrenaments/" + "n2/" + exp_name + "/"
-    
-    os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'  # os.environ["CUDA_VISIBLE_DEVICES"]="1"
+def custom_mse(y_true, y_pred):
+    mean_true = k.mean(y_true)
+    mean_pred = k.mean(y_pred)
+    alpha = mean_true / mean_pred
+    return K.mean(K.square(y_true - alpha * y_pred))
 
-    model = get_model(in_left=(in_size, in_size), in_right=(in_size, in_size))
 
-    model.compile(loss='mean_squared_error', optimizer=opt, metrics=['mse'])
+def train(exp_name, epochs, batch_size, img_size, n_train_img, n_val_img, n_test_img, conj, pretrained_weights, opt):
+    exp_path = "Entrenaments/" + exp_name + "/"
+    os.environ["CUDA_VISIBLE_DEVICES"] = "1"  # os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
+
+    model = get_model(in_left=(img_size, img_size), in_right=(img_size, img_size))
+
+    model.compile(loss=custom_mse, optimizer=opt, metrics=[custom_mse])
 
     if pretrained_weights:
-        model.load_weights(pretrained_weights)
+        model.load_weights(exp_path + pretrained_weights)
 
-    save_summary(model, exp_path)
+    save_summary(model, exp_path, exp_name)
 
-    train_generator = get_train_generator(main_path + "Images/Train/left",
-                                          main_path + "Images/Train/right",
-                                          main_path + "Images/Train/depth",
-                                          in_size,
+    train_generator = get_train_generator("Img" + str(img_size) + "_" + conj + "/Train/left",
+                                          "Img" + str(img_size) + "_" + conj + "/Train/right",
+                                          "Img" + str(img_size) + "_" + conj + "/Train/depth",
+                                          img_size,
                                           batch_size)
 
-    val_generator = get_train_generator(main_path + "Images/Validation/left",
-                                        main_path + "Images/Validation/right",
-                                        main_path + "Images/Validation/depth",
-                                        in_size,
+    val_generator = get_train_generator("Img" + str(img_size) + "_" + conj + "/Validation/left",
+                                        "Img" + str(img_size) + "_" + conj + "/Validation/right",
+                                        "Img" + str(img_size) + "_" + conj + "/Validation/depth",
+                                        img_size,
                                         batch_size)
+
+    es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=5)
 
     history = model.fit_generator(train_generator,
                                   steps_per_epoch=n_train_img / batch_size,
                                   epochs=epochs,
                                   validation_data=val_generator,
-                                  validation_steps=n_val_img / batch_size)
+                                  validation_steps=n_val_img / batch_size,
+                                  callbacks=[es])
 
-    save_validation(history.history, exp_path + "/loss.png")
+    # save_validation(history.history, exp_path + "/loss_" + exp_name + ".png")
+    model.save_weights(exp_path + "/weights_" + exp_name + ".h5")
 
-    test_generator = get_test_generator(main_path + "Images/Test/left",
-                                        main_path + "Images/Test/right",
-                                        in_size,
+    test_generator = get_test_generator("Img" + str(img_size) + "_" + conj + "/Test/left",
+                                        "Img" + str(img_size) + "_" + conj + "/Test/right",
+                                        img_size,
                                         1)
 
-    predictions = model.predict_generator(test_generator, steps=n_test_img)
-    predictions = predictions[:, :, :, 0]
-    save_predictions(exp_path + "Predictions", n_test_img, predictions)
+    pred = model.predict_generator(test_generator, steps=n_test_img)
+    pred = pred[:, :, :, 0]
+    save_predictions(exp_path + "Predictions_" + exp_name, n_test_img, pred)
+
+    return history
